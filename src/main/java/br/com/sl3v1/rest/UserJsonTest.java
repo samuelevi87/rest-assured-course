937e4a0da39aa04e350f4fa335cc07b00d46a147
samuelevi87 <samuel.levi.alves@outlook.com>
2022-02-24 18:23:21 -0300

    Validando o primeiro nível do Json com RestAssured + Hamcrest matchers e formas alternativas

---
+++ src/main/java/br/com/sl3v1/rest/UserJsonTest.java
@@ -0,0 +1,43 @@
+package br.com.sl3v1.rest;
+
+import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.request;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
+
+public class UserJsonTest {
+    @Test
+    public void deveVerificarPrimeiroNivel() {
+        given()
+        .when()
+                .get("https://restapi.wcaquino.me/users/1")
+        .then()
+                .statusCode(200)
+                .body("id", is(1))
+                .body("name", containsString("Silva"))
+                .body("age", greaterThan(31))
+        ;
+    }
+
+    @Test
+    public void deveVerificarPrimeiroNivelOutrasFormas() {
+        Response response = request(Method.GET, "https://restapi.wcaquino.me/users/1");
+        //path
+        assertEquals(Integer.valueOf(1), response.path("id"));
+        assertEquals(Integer.valueOf(1), response.path("%s", "id"));
+
+        //jsonpath
+        JsonPath jsonPath = new JsonPath((response.asString()));
+        assertEquals(1, jsonPath.getInt("id"));
+
+        //from
+
+        int id = JsonPath.from(response.asString()).getInt("id");
+        assertEquals(1, id);
+    }
+}
