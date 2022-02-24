7eb2e5ee1e963f577a6850625ba2597f13eea646
samuelevi87 <samuel.levi.alves@outlook.com>
2022-02-24 17:09:48 -0300

    Hamcrest matchers

--- src/main/java/br/com/sl3v1/rest/OlaMundoTest.java
+++ src/main/java/br/com/sl3v1/rest/OlaMundoTest.java
@@ -1,15 +1,16 @@
 package br.com.sl3v1.rest;
 
-import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
-import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.http.Method.GET;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 public class OlaMundoTest {
 
@@ -26,7 +27,7 @@ public class OlaMundoTest {
     }
 
     @Test
-    public void devoConhecerOutrasFormasRestAssured(){
+    public void devoConhecerOutrasFormasRestAssured() {
         Response response = request(GET, "http://restapi.wcaquino.me/ola");
         ValidatableResponse validacao = response.then();
         validacao.statusCode(200);
@@ -35,13 +36,36 @@ public class OlaMundoTest {
 
         given()
                 //Aqui vão as pré condições: quando vamos fazer uma requisição podemos mandar dados pelo cabeçalho e pelo corpo da mensagem, é aqui que esses dados são alocados.
-        .when()
+                .when()
                 //Aqui é onde a ação acontece, onde vão os métodos HTTP, GET, POST, PUT, DELETE, PATCH, OPTIONS
                 .get("http://restapi.wcaquino.me/ola")
-        .then()
+                .then()
                 .assertThat()
                 //Aqui é onde acontecem as verificações, inclusive com o encadeamento de várias delas.
                 .statusCode(201);
+
+    }
+
+    @Test
+    public void devoConhecerMatchersHamcrest() {
+        assertThat("Maria", is("Maria"));
+        assertThat(128, is(128));
+        assertThat(128, isA(Integer.class));
+        assertThat(12.8, isA(Double.class));
+        assertThat(12.8, greaterThan(11.5));
+        assertThat(12.8, lessThan(13.5));
+
+        List<Integer> impares = Arrays.asList(1, 3, 5, 7, 9, 11, 13, 15, 17, 19);
+        assertThat(impares, hasSize(10));
+        assertThat(impares, contains(1, 3, 5, 7, 9, 11, 13, 15, 17, 19)); //Deve estar ordenado e ter todos os itens
+        assertThat(impares, containsInAnyOrder(1, 5, 3, 15, 9, 13, 11, 17, 7, 19));
+        assertThat(impares, hasItem(19));
+        assertThat(impares, hasItems(19, 7, 1));
+
+        assertThat("Maria", is(not("João")));
+        assertThat("Maria", not("João"));
+        assertThat("Mary", anyOf(is("Marina"), is("Mary")));
+        assertThat("Jolibedina", allOf(startsWith("Jo"), endsWith("na"), containsString("be")));
 
     }
 
